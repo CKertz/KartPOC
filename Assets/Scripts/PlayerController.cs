bdecce78367baf7da0e73a26b8be79e0d205b5fa
Cooper Kertz <kertzc@live.com>
2024-03-05 22:23:40 -0600

    finished basic implementation of trail collision, removed the old implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -88,63 +88,8 @@ public class PlayerController : MonoBehaviour
     {
         //check if the surface in contact is a new surface. add to list if so and keep updating while on top of it
         //use this https://www.youtube.com/watch?v=aPXvoWVabPY to help with surface handling
-
-        List<Vector2> currentCellsCovered = GetCurrentHarvesterCoveredPositions();
         
-        foreach (Vector2 cell in currentCellsCovered)
-        {
-            Debug.Log("scanning cell with coords:" + cell.x + "," + cell.y);
-            if (!visitedPositions.Contains(cell))
-            {
-                visitedPositions.Add(cell);
-            }
-        }
         //surfaces[0].totalScore += totalDistanceTraveled * surfaces[0].scoreModifier;
-    }
-
-    private List<Vector2> GetCurrentHarvesterCoveredPositions()
-    {
-        List<Vector2> edgeCoordinates = new List<Vector2>();
-
-        // Get the sprite's bounds
-        Bounds spriteBounds = harvestOutlineSprite.bounds;
-
-        // Get the position of the sprite
-        Vector3 spritePosition = harvestOutlineSprite.transform.position;
-
-        // Calculate edges
-        
-        float leftEdge = (float)Math.Round(spritePosition.x - spriteBounds.extents.x, 1);
-        float rightEdge = (float)Math.Round(spritePosition.x + spriteBounds.extents.x, 1);
-        float bottomEdge = (float)Math.Round(spritePosition.y - spriteBounds.extents.y, 1);
-        float topEdge = (float)Math.Round(spritePosition.y + spriteBounds.extents.y, 1);
-
-        // Print or use the edge coordinates
-        //Debug.Log("Left Edge: " + leftEdge);
-        //Debug.Log("Right Edge: " + rightEdge);
-        //Debug.Log("Bottom Edge: " + bottomEdge);
-        //Debug.Log("Top Edge: " + topEdge);
-
-        for (float x = leftEdge; x <= rightEdge; x++)
-        {
-            // Calculate corresponding y-coordinate for each x-coordinate
-            float y = spritePosition.y + (spriteBounds.extents.y * Mathf.Sin(Mathf.Acos((x - spritePosition.x) / spriteBounds.extents.x)));
-            edgeCoordinates.Add(new Vector2(x, y));
-        }
-
-        return edgeCoordinates;
-
-    }
-
-
-    private Vector2 WorldToCell(Vector2 worldPos)
-    {
-        // rounding it to 1 decimal point for now, if more precision is needed it can always be adjusted
-        float x = (float)Math.Round(worldPos.x * cellSize, 1);
-        float y = (float)Math.Round(worldPos.y * cellSize, 1);
-        //Debug.Log("x:" + x+ " Y:"+y);
-
-        return new Vector2(x, y);
     }
 
     private void UpdateHarvesterRotation(float horizontalInput, float verticalInput)
--- Assets/TrailColliderController.cs
+++ Assets/TrailColliderController.cs
@@ -4,35 +4,24 @@ using UnityEngine;
 
 public class TrailColliderController : MonoBehaviour
 {
-    bool isHit = false;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log("trail hit:" + isHit + "by: " + collidingObjectName);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("collision entered by " + collision.name);
-        //if (collision.gameObject.name == "Player")
-        //{
-        //    isHit = true;
-        //}
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("collision exited by " + collision.name);
-        //collidingObjectName = collision.name;
-        //if (collision.gameObject.name == "Player")
-        //{
-        //    isHit = false;
-        //}
     }
 }
