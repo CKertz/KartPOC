a22d3921d4284ecc9aaed006e9e70e512f58fcdd
Cooper Kertz <kertzc@live.com>
2024-05-02 21:18:16 -0500

    added some foundation for floor generation

---
+++ Assets/_Scripts/Managers/StageManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateStageLayout()
+    {
+        // on starting game/entering a new stage, procedurally generate a floor layout
+    }
+}
